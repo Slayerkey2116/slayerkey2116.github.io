d7687b766322e09f2939fda1c1e9110ef443f1d7
Voshon Key <voshonk@yahoo.com>
2022-02-20 14:22:55 -0500

    Uploaded .html project

---
+++ Main.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+const navToggle = document.querySelector('.navbar-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove('nav-open');
+    });
+});
